ab908fcea349a487ebe48c4bac835f0c7bb2c7e8
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2023-10-15 17:35:10 +0900

    通知設定

--- app/home.tsx
+++ app/home.tsx
@@ -1,7 +1,46 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+const schedulePushNotification = async () => {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 5 },
+  });
+};
 
 export default function Home() {
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+
+  useEffect(() => {
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => setNotification(notification),
+    );
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
@@ -9,6 +48,17 @@ export default function Home() {
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
+      />
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>Title: {notification?.request.content.title} </Text>
+        <Text>Body: {notification?.request.content.body}</Text>
+        <Text>Data: {JSON.stringify(notification?.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
       />
     </View>
   );
